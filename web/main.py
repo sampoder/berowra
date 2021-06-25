d8b47b04b811750b20c78098a70f20860303859a
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-06-25 18:59:49 +0800

    Work on the Frontend + Tailwind System

--- web/main.py
+++ web/main.py
@@ -1,6 +1,14 @@
 from flask import Flask, request, redirect, url_for, render_template
+from deta import Deta
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = Flask(__name__)
+
+deta = Deta(os.getenv("DETA_KEY"))
+
+collectionsDB = deta.Base("collections")
 
 @app.route('/', methods=["GET"])
 def hello_world():
