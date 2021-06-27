eea11672e4fdcb4bf5091c82fb7e9d4b7c54bc24
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-06-27 12:22:07 +0800

    Updates to Frontend + New Route

--- web/main.py
+++ web/main.py
@@ -11,7 +11,20 @@ deta = Deta(os.getenv("DETA_KEY"))
 collectionsDB = deta.Base("collections")
 
 @app.route('/', methods=["GET"])
-def hello_world():
+def index():
+    items = collectionsDB.fetch(pages=1, buffer=50000)
+    for sub_list in items: 
+        items = sub_list
+    print(items)
+    return render_template('index.html', collections=items)
+
+@app.route('/new', methods=["GET"])
+def new():
+    collectionsDB.insert({
+        "title": "Blog Posts",
+        "templateItems": [],
+        "description": "Blogs to be posted on sampoder.com"
+    })
     return render_template('index.html')
 
 if __name__ == "__main__":
